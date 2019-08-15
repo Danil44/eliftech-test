7efb397b47f132a5de6945824f4748a6cc8e576c
unknown <lie4light@gmail.com>
2019-08-16 01:59:24 +0300

    try to dix scripts for deploy

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app
 app.listen(process.env.PORT || port);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(expess.static("client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
